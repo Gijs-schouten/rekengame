5e0fc5dd07ceb3aa9c496648e14709fa787c7425
Gijs Schouten <21969@ma-web.nl>
2017-10-13 12:17:38 +0200

    *

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = require("http").createServer(app);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(__dirname+'/index.html');
 });
 
 server.listen(port);
